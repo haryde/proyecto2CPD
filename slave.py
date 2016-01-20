46e42be3b0ae8773e60448d26d0998be9712dc86
haryde <haryde91@gmail.com>
2016-01-20 19:06:35 +0100

    Create slave.py

---
+++ slave.py
@@ -0,0 +1,56 @@
+import sqlite3                        # Database
+import time                           # For sleep
+import yahooDataObject
+import Pyro4
+
+f = time.strftime("%d/%m/%Y")
+h = time.strftime("%H:%M:%S")
+fecha = "'" + f + "'"
+hora = "'" + h + "'"
+
+conn = sqlite3.connect('slaveDB.db') # Create connection
+c = conn.cursor() # Create cursor
+
+class SlaveProgram:
+    def __init__(self, a_socket):
+        self._objList = [yahooDataObject.yahooDataObject('GOOG'), 
+                                            yahooDataObject.yahooDataObject('AAPL'),
+                                            yahooDataObject.yahooDataObject('IBM'),
+                                            yahooDataObject.yahooDataObject('MSFT'),
+                                            yahooDataObject.yahooDataObject('TOSHBF'),
+                                            yahooDataObject.yahooDataObject('SNE'),
+                                            yahooDataObject.yahooDataObject('2357.TW'),
+                                            yahooDataObject.yahooDataObject('LNVGY'),
+                                            yahooDataObject.yahooDataObject('ITX.MC'),
+                                            yahooDataObject.yahooDataObject('SAN'),
+                                            yahooDataObject.yahooDataObject('FB'),
+                                            yahooDataObject.yahooDataObject('IDR.MC')]
+        
+    def getBootTime(self):
+        return hora
+    
+    def dataBase(self):
+        # c.execute('''DROP TABLE IF EXISTS stocks''')        
+        c.execute('CREATE TABLE if not EXISTS stocks (symbol text, last text, xdate text, change text, high text, low text, vol text, send text)')
+
+    def doWork(self):
+        for obj in self._objList:
+            c = conn.cursor()
+            obj.updateYahooStockQuoteWeb()
+            valores = obj.D
+            
+            query = 'INSERT INTO stocks (symbol, last, xdate, change, high, low, vol, send) VALUES (:symbol, :last, :date, :change, :high, :low, :vol, 1);'
+            c.execute(query, valores)
+            print c.fetchall()
+            conn.commit()
+
+daemon = Pyro4.Daemon()
+ns = Pyro4.locateNS()
+uri = daemon.register(SlaveProgram)
+ns.register("slave.boot.time", uri)
+
+print ("Slave Booted")
+daemon.requestLoop()
+    
+    
+     
